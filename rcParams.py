b451b7addd1f17b8272959e55cba12c68ada0b7f
Bernhard Kaplan <bernhard.kaplan@gmail.com>
2013-04-02 11:19:21 +0200

    rcParams added

---
+++ rcParams.py
@@ -0,0 +1,7 @@
+rcParams = { 'axes.labelsize' : 28,
+            'label.fontsize': 28,
+            'xtick.labelsize' : 24, 
+            'ytick.labelsize' : 24, 
+            'axes.titlesize'  : 32,
+            'legend.fontsize': 9}
+
